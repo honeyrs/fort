b18fe8d2bc63cb72006b8a50e5bfbcd6bbf80157
honeyrs <rsnaam00@gmail.com>
2025-04-11 02:49:48 +0530

    Update settings.py

--- plugins/settings.py
+++ plugins/settings.py
@@ -102,8 +102,198 @@ async def settings_query(bot, query):
             "<b><u>My Channels</b></u>\n\n<b>You can manage your target chats in here</b>",
             reply_markup=InlineKeyboardMarkup(buttons))
    
-    elif type == " Sinh
+    elif type == "addchannel":  
+        await query.message.delete()
+        try:
+            text = await bot.send_message(user_id, "<b>❪ SET TARGET CHAT ❫\n\nForward a message from Your target chat\n/cancel - cancel this process</b>")
+            chat_ids = await bot.listen(chat_id=user_id, timeout=300)
+            if chat_ids.text == "/cancel":
+                await chat_ids.delete()
+                return await text.edit_text(
+                    "<b>process canceled</b>",
+                    reply_markup=InlineKeyboardMarkup(buttons))
+            elif not chat_ids.forward_date:
+                await chat_ids.delete()
+                return await text.edit_text("**This is not a forward message**")
+            else:
+                chat_id = chat_ids.forward_from_chat.id
+                title = chat_ids.forward_from_chat.title
+                username = chat_ids.forward_from_chat.username
+                username = "@" + username if username else "private"
+            chat = await db.add_channel(user_id, chat_id, title, username)
+            await chat_ids.delete()
+            await text.edit_text(
+                "<b>Successfully updated</b>" if chat else "<b>This channel already added</b>",
+                reply_markup=InlineKeyboardMarkup(buttons))
+        except asyncio.exceptions.TimeoutError:
+            await text.edit_text('Process has been automatically cancelled', reply_markup=InlineKeyboardMarkup(buttons))
+    
+    elif type.startswith("editchannels"): 
+        chat_id = type.split('_')[1]
+        channel = await db.get_channel_details(user_id, int(chat_id))
+        if not channel:
+            await query.message.edit_text("Channel not found", reply_markup=InlineKeyboardMarkup(buttons))
+            return
+        buttons = [[InlineKeyboardButton('❌ Remove ❌', callback_data=f"settings#removechannel_{chat_id}")],
+                  [InlineKeyboardButton('↩ Back', callback_data="settings#channels")]]
+        await query.message.edit_text(
+            f"<b>Channel Details</b>\n\nTitle: {channel['title']}\nChat ID: {chat_id}\nUsername: {channel['username']}",
+            reply_markup=InlineKeyboardMarkup(buttons))
+    
+    elif type.startswith("removechannel"):
+        chat_id = type.split('_')[1]
+        await db.remove_channel(user_id, int(chat_id))
+        await query.message.edit_text(
+            "<b>Successfully removed channel</b>",
+            reply_markup=InlineKeyboardMarkup(buttons))
+    
+    elif type == "caption":
+        buttons = []
+        caption = (await get_configs(user_id))['caption']
+        if caption is None:
+            buttons.append([InlineKeyboardButton('✚ Add Caption ✚', 
+                          callback_data="settings#addcaption")])
+        else:
+            buttons.append([InlineKeyboardButton('See Caption', 
+                          callback_data="settings#seecaption")])
+            buttons[-1].append(InlineKeyboardButton('🗑️ Delete Caption', 
+                          callback_data="settings#deletecaption"))
+        buttons.append([InlineKeyboardButton('↩ Back', 
+                        callback_data="settings#main")])
+        await query.message.edit_text(
+            "<b><u>CUSTOM CAPTION</b></u>\n\n<b>You can set a custom caption to videos and documents. Normaly use its default caption</b>\n\n<b><u>AVAILABLE FILLINGS:</b></u>\n- <code>{filename}</code> : Filename\n- <code>{size}</code> : File size\n- <code>{caption}</code> : default caption",
+            reply_markup=InlineKeyboardMarkup(buttons))
+                               
+    elif type == "seecaption":   
+        data = await get_configs(user_id)
+        buttons = [[InlineKeyboardButton('🖋️ Edit Caption', 
+                    callback_data="settings#addcaption")
+                   ],[
+                   InlineKeyboardButton('↩ Back', 
+                     callback_data="settings#caption")]]
+        await query.message.edit_text(
+            f"<b><u>YOUR CUSTOM CAPTION</b></u>\n\n<code>{data['caption']}</code>",
+            reply_markup=InlineKeyboardMarkup(buttons))
+    
+    elif type == "deletecaption":
+        await update_configs(user_id, 'caption', None)
+        await query.message.edit_text(
+            "<b>successfully updated</b>",
+            reply_markup=InlineKeyboardMarkup(buttons))
+                              
+    elif type == "addcaption":
+        await query.message.delete()
+        try:
+            text = await bot.send_message(query.message.chat.id, "Send your custom caption\n/cancel - <code>cancel this process</code>")
+            caption = await bot.listen(chat_id=user_id, timeout=300)
+            if caption.text == "/cancel":
+                await caption.delete()
+                return await text.edit_text(
+                      "<b>process canceled !</b>",
+                      reply_markup=InlineKeyboardMarkup(buttons))
+            try:
+                caption.text.format(filename='', size='', caption='')
+            except KeyError as e:
+                await caption.delete()
+                return await text.edit_text(
+                   f"<b>wrong filling {e} used in your caption. change it</b>",
+                   reply_markup=InlineKeyboardMarkup(buttons))
+            await update_configs(user_id, 'caption', caption.text)
+            await caption.delete()
+            await text.edit_text(
+                "<b>successfully updated</b>",
+                reply_markup=InlineKeyboardMarkup(buttons))
+        except asyncio.exceptions.TimeoutError:
+            await text.edit_text('Process has been automatically cancelled', reply_markup=InlineKeyboardMarkup(buttons))
+  
+    elif type == "button":
+        buttons = []
+        button = (await get_configs(user_id))['button']
+        if button is None:
+            buttons.append([InlineKeyboardButton('✚ Add Button ✚', 
+                          callback_data="settings#addbutton")])
+        else:
+            buttons.append([InlineKeyboardButton('👀 See Button', 
+                          callback_data="settings#seebutton")])
+            buttons[-1].append(InlineKeyboardButton('🗑️ Remove Button ', 
+                          callback_data="settings#deletebutton"))
+        buttons.append([InlineKeyboardButton('↩ Back', 
+                        callback_data="settings#main")])
+        await query.message.edit_text(
+            "<b><u>CUSTOM BUTTON</b></u>\n\n<b>You can set a inline button to messages.</b>\n\n<b><u>FORMAT:</b></u>\n`[Forward bot][buttonurl:https://t.me/devgaganbot]`\n",
+            reply_markup=InlineKeyboardMarkup(buttons))
+  
+    elif type == "addbutton":
+        await query.message.delete()
+        try:
+            txt = await bot.send_message(user_id, text="**Send your custom button.\n\nFORMAT:**\n`[forward bot][buttonurl:https://t.me/devgaganbot]`\n")
+            ask = await bot.listen(chat_id=user_id, timeout=300)
+            button = parse_buttons(ask.text.html)
+            if not button:
+                await ask.delete()
+                return await txt.edit_text("**INVALID BUTTON**")
+            await update_configs(user_id, 'button', ask.text.html)
+            await ask.delete()
+            await txt.edit_text("**Successfully button added**",
+                reply_markup=InlineKeyboardMarkup(buttons))
+        except asyncio.exceptions.TimeoutError:
+            await txt.edit_text('Process has been automatically cancelled', reply_markup=InlineKeyboardMarkup(buttons))
+  
+    elif type == "seebutton":
+        button = (await get_configs(user_id))['button']
+        button = parse_buttons(button, markup=False)
+        button.append([InlineKeyboardButton("↩ Back", "settings#button")])
+        await query.message.edit_text(
+            "**YOUR CUSTOM BUTTON**",
+            reply_markup=InlineKeyboardMarkup(button))
+      
+    elif type == "deletebutton":
+        await update_configs(user_id, 'button', None)
+        await query.message.edit_text(
+            "**Successfully button deleted**",
+            reply_markup=InlineKeyboardMarkup(buttons))
+   
+    elif type == "database":
+        buttons = []
+        db_uri = (await get_configs(user_id))['db_uri']
+        if db_uri is None:
+            buttons.append([InlineKeyboardButton('✚ Add Url ✚', 
+                          callback_data="settings#addurl")])
+        else:
+            buttons.append([InlineKeyboardButton('👀 See Url', 
+                          callback_data="settings#seeurl")])
+            buttons[-1].append(InlineKeyboardButton('🗑️ Remove Url ', 
+                          callback_data="settings#deleteurl"))
+        buttons.append([InlineKeyboardButton('↩ Back', 
+                        callback_data="settings#main")])
+        await query.message.edit_text(
+            "<b><u>DATABASE</u>\n\nDatabase is required for store your duplicate messages permenant. other wise stored duplicate media may be disappeared when after bot restart.</b>",
+            reply_markup=InlineKeyboardMarkup(buttons))
 
+    elif type == "addurl":
+        await query.message.delete()
+        uri = await bot.ask(user_id, "<b>please send your mongodb url.</b>\n\n<i>get your Mongodb url from [here](https://mongodb.com)</i>", disable_web_page_preview=True)
+        if uri.text == "/cancel":
+            return await uri.reply_text(
+                      "<b>process canceled !</b>",
+                      reply_markup=InlineKeyboardMarkup(buttons))
+        if not uri.text.startswith("mongodb+srv://") and not uri.text.endswith("majority"):
+            return await uri.reply("<b>Invalid Mongodb Url</b>",
+                       reply_markup=InlineKeyboardMarkup(buttons))
+        await update_configs(user_id, 'db_uri', uri.text)
+        await uri.reply("**Successfully database url added**",
+                reply_markup=InlineKeyboardMarkup(buttons))
+  
+    elif type == "seeurl":
+        db_uri = (await get_configs(user_id))['db_uri']
+        await query.answer(f"DATABASE URL: {db_uri}", show_alert=True)
+  
+    elif type == "deleteurl":
+        await update_configs(user_id, 'db_uri', None)
+        await query.message.edit_text(
+            "**Successfully your database url deleted**",
+            reply_markup=InlineKeyboardMarkup(buttons))
+      
     elif type == "filters":
         logger.info(f"Opening filters menu for user {user_id}")
         try:
@@ -198,6 +388,7 @@ async def settings_query(bot, query):
         await update_configs(user_id, 'extension', None)
         await query.message.edit_text(text="**successfully deleted**",
                                        reply_markup=InlineKeyboardMarkup(buttons))
+    
     elif type == "add_keyword":
         await query.message.delete()
         ask = await bot.ask(user_id, text="**please send the keywords (seperete by space)**")
@@ -231,6 +422,7 @@ async def settings_query(bot, query):
         await update_configs(user_id, 'keywords', None)
         await query.message.edit_text(text="**successfully deleted**",
                                        reply_markup=InlineKeyboardMarkup(buttons))
+    
     elif type.startswith("alert"):
         alert = type.split('_')[1]
         await query.answer(alert, show_alert=True)
